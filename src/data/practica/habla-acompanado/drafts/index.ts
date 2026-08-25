35eceb3269375e326c9efecedd1ebaefe71c1d4c
Jose David Duarte Silva <josedavidduartesilva@Joses-MacBook-Air.local>
2026-08-25 10:07:39 -0500

    feat(practica): complete German A2 roleplays

--- src/data/practica/habla-acompanado/drafts/index.ts
+++ src/data/practica/habla-acompanado/drafts/index.ts
@@ -90,6 +90,7 @@ import { ROLEPLAY_ALEMAN_A2_BATCH_4 } from './aleman-a2-batch-4.ts'
 import { ROLEPLAY_ALEMAN_A2_BATCH_4B } from './aleman-a2-batch-4b.ts'
 import { ROLEPLAY_ALEMAN_A2_BATCH_4C } from './aleman-a2-batch-4c.ts'
 import { ROLEPLAY_ALEMAN_A2_BATCH_4D } from './aleman-a2-batch-4d.ts'
+import { ROLEPLAY_ALEMAN_A2_BATCH_4E } from './aleman-a2-batch-4e.ts'
 
 const ENGLISH_A2_PUBLISHED_SEQUENCE: Record<string, number> = {
   'the-bike-in-the-parking-lot': 1,
@@ -220,6 +221,7 @@ export const ROLEPLAY_ALEMAN_A2_CANDIDATE = [
   ...ROLEPLAY_ALEMAN_A2_BATCH_4B,
   ...ROLEPLAY_ALEMAN_A2_BATCH_4C,
   ...ROLEPLAY_ALEMAN_A2_BATCH_4D,
+  ...ROLEPLAY_ALEMAN_A2_BATCH_4E,
 ].sort((a, b) => a.sequence - b.sequence)
 
 /**
